a162417f1d43d53ef0a4bf5217b7d8a07892a6f9
Mark Garrison <garrison.gaming@gmail.com>
2014-04-28 14:22:18 -0400

    Override ToString for the bound Part object for the Telerik app
    This will set the UIA name field value.

--- telerik-testing-app/TelerikWpfApp/Part.cs
+++ telerik-testing-app/TelerikWpfApp/Part.cs
@@ -18,5 +18,10 @@ namespace TelerikWpfApp
         public string PartId { get; set; }
         public string Description { get; set; }
         public int Quantity { get; set; }
+
+        public override string ToString()
+        {
+            return PartId;
+        }
     }
 }
